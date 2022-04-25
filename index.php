c54b78b83a1a6b94ed182e1127b8c76d9110d8ce
Viktor Karpinski <viktorkarpinski@Viktors-MacBook-Pro-2.local>
2022-04-25 16:56:06 +0200

    v1.0.1; Some Frontend;

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&family=Open+Sans&display=swap" rel="stylesheet">
+
+    <title>
+        USARPS
+    </title>
+</head>
+
+<body>
+    <header class="box">
+        <h1>
+            USARPS
+        </h1>
+        <p>
+            The United States of America Rock Paper Scissors League was a national competition league for the hand game rock paper scissors.
+        </p>
+    </header>
+
+    <div class="text-box">
+        <h1>
+            Latest Games
+        </h1>
+    </div>
+
+    <article class="box">
+
+    </article>
+</body>
+
+</html>
